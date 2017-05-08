2c68db9dd665dd2e486d303f7b6433da7e8f0a32
TunjiTofu <adetunjitofunmi@gmail.com>
2017-05-08 04:25:55 +0100

    reg

--- application/modules/Users/views/signup_v.php
+++ application/modules/Users/views/signup_v.php
@@ -39,7 +39,7 @@
                             <i class="material-icons">person</i>
                         </span>
                     <div class="form-line">
-                        <input type="text" class="form-control" name="username" placeholder="Usernmae" required autofocus>
+                        <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
                     </div>
                 </div>
                 <div class="input-group">

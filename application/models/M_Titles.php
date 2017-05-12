36701bb89c0d64a5d7bcab4cbc717743e3f3c2c3
EbenezerOyenuga <ebene.oyen@yahoo.com>
2017-05-12 04:39:33 +0100

    Vehicle Owner Signup Now Working

---
+++ application/models/M_Titles.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ebene
+ * Date: 12/1/2016
+ * Time: 11:30 AM
+ */
+class M_Titles extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    function get_active_titles(){
+
+        $query = $this->db->get('tbl_titles');
+
+        return $query->result();
+    }
+
+    function get_title(){
+
+        $query = $this->db->get('tbl_titles');
+
+        return $query->result();
+    }
+}
